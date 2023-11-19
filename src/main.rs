129a467772e238ec973cd9a8d8a2d6a88d4d0055
tomara_x <86204514+tomara-x@users.noreply.github.com>
2023-11-19 06:37:03 +0200

    more circles and confusing things

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
+use std::f32::consts::TAU;
+
 use bevy::prelude::*;
 use bevy_vector_shapes::prelude::*;
 use bevy_pancam::{PanCam, PanCamPlugin};
+use bevy::render::camera::ScalingMode;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(PanCamPlugin::default())
         .add_plugins(Shape2dPlugin::default())
+        .insert_resource(ClearColor(Color::DARK_GRAY))
+        .insert_resource(Msaa::Off) //hm?
         .add_systems(Startup, setup)
-        .add_systems(Update, draw)
+        .add_systems(Update, draw_gallery)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     // Spawn the camera
     commands.spawn((
-        Camera2dBundle::default(),
+        Camera2dBundle {
+            transform: Transform::from_translation(Vec3::Z), //HM?!
+            projection: OrthographicProjection {
+                scaling_mode: ScalingMode::AutoMin { //HMM??
+                    min_width: 5.2 * 4.5,
+                    min_height: 3.2 * 4.5,
+                },
+                ..default()
+            },
+        ..default()
+        },
         PanCam {
             //limit zooming
             max_scale: Some(40.),
@@ -25,7 +40,36 @@ fn setup(mut commands: Commands) {
     ));
 }
 
-fn draw(mut painter: ShapePainter) {
-    // Draw a circle
-    painter.circle(100.0);
+fn draw_circles(painter: &mut ShapePainter, radius: f32) {
+    painter.translate(-(Vec3::X + Vec3::NEG_Y) * f32::sqrt(radius) * 0.5);
+    painter.color = Color::rgba(1.0, 0.0, 0.0, 0.5);
+    painter.circle(radius);
+
+    painter.rotate_z(-TAU / 3.0);
+    painter.translate(Vec3::Y * radius * 1.2 + Vec3::Z * 0.0001);
+    painter.color = Color::rgba(0.0, 1.0, 0.0, 0.5);
+    painter.circle(radius);
+
+    painter.rotate_z(-TAU / 3.0);
+    painter.translate(Vec3::Y * radius * 1.2 + Vec3::Z * 0.0001);
+    painter.color = Color::rgba(0.0, 0.0, 1.0, 0.5);
+    painter.circle(radius);
+}
+
+fn draw_gallery(mut painter: ShapePainter) {
+    let radius = 2.0;
+
+    painter.reset();
+    painter.translate(Vec3::X * radius * -4.0);
+    painter.alpha_mode = AlphaMode::Add;
+    draw_circles(&mut painter, radius);
+
+    painter.reset();
+    painter.alpha_mode = AlphaMode::Multiply;
+    draw_circles(&mut painter, radius);
+
+    painter.reset();
+    painter.translate(Vec3::X * radius * 4.0);
+    painter.alpha_mode = AlphaMode::Blend;
+    draw_circles(&mut painter, radius);
 }
