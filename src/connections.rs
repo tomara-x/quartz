4a464b409922e0065734a3a082a2bccd0cb5a692
tomara_x <86204514+tomara-x@users.noreply.github.com>
2023-12-09 23:31:08 +0200

    make connection system (not working yet)

--- src/connections.rs
+++ src/connections.rs
@@ -7,10 +7,11 @@ pub struct ConnectionsPlugin;
 
 impl Plugin for ConnectionsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, add_connection_component);
+        app.add_systems(Update, create_connection);
     }
 }
 
+#[derive(Debug)]
 enum Target {
     Color,
     Pos,
@@ -19,7 +20,7 @@ enum Target {
     Radius,
 }
 
-#[derive(Component)]
+#[derive(Component, Debug)]
 struct Connection {
     src: Vec<Entity>,
     dst: Vec<Entity>,
@@ -33,23 +34,46 @@ struct Add;
 #[derive(Component)]
 struct Mult;
 
-fn add_connection_component(
+//not working
+fn create_connection(
     keyboard_input: Res<Input<KeyCode>>,
     mouse_button_input: Res<Input<MouseButton>>,
     mut commands: Commands,
-    query: Query<(Entity, &Radius, &Pos), With<Visible>>,
+    mut query: Query<(Entity, &Radius, &Pos, Option<&mut Connection>), With<Visible>>,
     cursor: Res<CursorInfo>,
 ) {
     let ctrl = keyboard_input.any_pressed([KeyCode::ControlLeft, KeyCode::ControlRight]);
     if ctrl && mouse_button_input.just_pressed(MouseButton::Left) {
-        for (e, r, p) in query.iter() {
+        for (e, r, p, mut c) in query.iter_mut() {
             if cursor.i.distance(p.value.xy()) < r.value {
-                commands.entity(e).insert(Connection {
-                    src: vec![e],
-                    dst: vec![e],
-                    src_target: Target::Color,
-                    dst_target: Target::Color,
-                });
+                match c {
+                    None => {commands.entity(e).insert(Connection {
+                                src: vec![e],
+                                dst: vec![],
+                                src_target: Target::Color,
+                                dst_target: Target::Color,
+                            });
+                    ()},
+                    Some(ref mut connection) => connection.src.push(e),
+                }
+                break;
+            }
+        }
+    }
+    if ctrl && mouse_button_input.just_released(MouseButton::Left) {
+        for (e, r, p, mut c) in query.iter_mut() {
+            if cursor.f.distance(p.value.xy()) < r.value {
+                match c {
+                    None => {commands.entity(e).insert(Connection {
+                                src: vec![],
+                                dst: vec![e],
+                                src_target: Target::Color,
+                                dst_target: Target::Color,
+                            });
+                    ()},
+                    Some(ref mut connection) => connection.src.push(e),
+                }
+                println!("{:?}", c);
                 break;
             }
         }
