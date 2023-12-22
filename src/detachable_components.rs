b37aa802903265d8d852003fcb659772cc433f32
tomara_x <86204514+tomara-x@users.noreply.github.com>
2023-12-22 05:48:39 +0200

    add detachable components using f keys

--- src/detachable_components.rs
+++ src/detachable_components.rs
@@ -12,90 +12,90 @@ impl Plugin for DetachableComponentsPlugin {
         app.register_type::<ColorOffset>();
         app.register_type::<PosOffset>();
         app.register_type::<RadiusOffset>();
-        app.add_systems(Update, attach_data.run_if(in_state(Mode::Edit)));
-        app.add_systems(Update, detach_data.run_if(in_state(Mode::Edit)));
+        app.add_systems(Update, attach_detach_data.run_if(in_state(Mode::Edit)));
     }
 }
 
-#[derive(Component, Reflect)]
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
 struct Num(f32);
 
-#[derive(Component, Reflect)]
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
 struct Arr(Vec<f32>);
 
-#[derive(Component, Reflect)]
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
 struct ColorOffset(Color);
 
-#[derive(Component, Reflect)]
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
 struct PosOffset(Vec3);
 
-#[derive(Component, Reflect)]
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
 struct RadiusOffset(f32);
 
-fn attach_data(
+fn attach_detach_data(
     keyboard_input: Res<Input<KeyCode>>,
     query: Query<Entity, With<Selected>>,
     mut commands: Commands,
 ) {
-    if keyboard_input.any_pressed([KeyCode::ControlLeft, KeyCode::ControlRight]) {
-        if keyboard_input.just_pressed(KeyCode::N) {
+    let shift = keyboard_input.any_pressed([KeyCode::ShiftLeft, KeyCode::ShiftRight]);
+    if keyboard_input.just_pressed(KeyCode::F1) {
+        if shift {
+            for e in query.iter() {
+                commands.entity(e).remove::<Num>();
+            }
+        } else {
             for e in query.iter() {
                 commands.entity(e).insert(Num(0.0));
             }
         }
-        if keyboard_input.just_pressed(KeyCode::A) {
+    }
+    if keyboard_input.just_pressed(KeyCode::F2) {
+        if shift {
+            for e in query.iter() {
+                commands.entity(e).remove::<Arr>();
+            }
+        } else {
             for e in query.iter() {
                 commands.entity(e).insert(Arr(vec![0.,1.,2.,4.]));
             }
         }
-        if keyboard_input.just_pressed(KeyCode::C) {
+    }
+    if keyboard_input.just_pressed(KeyCode::F3) {
+        if shift {
+            for e in query.iter() {
+                commands.entity(e).remove::<ColorOffset>();
+            }
+        } else {
             for e in query.iter() {
                 commands.entity(e).insert(ColorOffset(Color::hsl(0.0,1.0,0.5)));
             }
         }
-        if keyboard_input.just_pressed(KeyCode::P) {
+    }
+    if keyboard_input.just_pressed(KeyCode::F4) {
+        if shift {
+            for e in query.iter() {
+                commands.entity(e).remove::<PosOffset>();
+            }
+        } else {
             for e in query.iter() {
                 commands.entity(e).insert(PosOffset(Vec3::ONE));
             }
         }
-        if keyboard_input.just_pressed(KeyCode::R) {
+    }
+    if keyboard_input.just_pressed(KeyCode::F5) {
+        if shift {
+            for e in query.iter() {
+                commands.entity(e).remove::<RadiusOffset>();
+            }
+        } else {
             for e in query.iter() {
                 commands.entity(e).insert(RadiusOffset(1.));
             }
         }
     }
 }
-
-fn detach_data(
-    keyboard_input: Res<Input<KeyCode>>,
-    query: Query<Entity, With<Selected>>,
-    mut commands: Commands,
-) {
-    if keyboard_input.just_pressed(KeyCode::Key1) {
-        for e in query.iter() {
-            commands.entity(e).remove::<Num>();
-        }
-    }
-    if keyboard_input.just_pressed(KeyCode::Key2) {
-        for e in query.iter() {
-            commands.entity(e).remove::<Arr>();
-        }
-    }
-    if keyboard_input.just_pressed(KeyCode::Key3) {
-        for e in query.iter() {
-            commands.entity(e).remove::<ColorOffset>();
-        }
-    }
-    if keyboard_input.just_pressed(KeyCode::Key4) {
-        for e in query.iter() {
-            commands.entity(e).remove::<PosOffset>();
-        }
-    }
-    if keyboard_input.just_pressed(KeyCode::Key5) {
-        for e in query.iter() {
-            commands.entity(e).remove::<RadiusOffset>();
-        }
-    }
-}
-
 
