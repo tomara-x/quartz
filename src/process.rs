42e63c633204d073bdb5b5ca40c4109a1903f97f
tomara_x <86204514+tomara-x@users.noreply.github.com>
2024-01-03 20:42:56 +0200

    the good, the bad, and the ridiculous

--- src/process.rs
+++ src/process.rs
@@ -7,11 +7,13 @@ use bevy::{
         },
     prelude::*};
 
-use crossbeam_channel::{bounded, Receiver};
-use knyst::{
-    audio_backend::{CpalBackend, CpalBackendOptions},
-    prelude::*,
-};
+use crossbeam_channel::bounded;
+
+use assert_no_alloc::*;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{FromSample, SizedSample};
+use fundsp::hacker as dsp;
+use fundsp::audiounit::AudioUnit64;
 
 use crate::components::*;
 
@@ -30,13 +32,72 @@ macro_rules! mark_changed {
 pub fn ext_thread(mut commands: Commands) {
     let (tx, rx) = bounded::<f32>(10);
     std::thread::spawn(move || {
+        let host = cpal::default_host();
+        let device = host
+            .default_output_device()
+            .expect("Failed to find a default output device");
+        let config = device.default_output_config().unwrap();
+        match config.sample_format() {
+            cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()).unwrap(),
+            cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()).unwrap(),
+            cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()).unwrap(),
+            _ => panic!("Unsupported format"),
+        }
         loop {
             if let Ok(input) = rx.recv() {
-                println!("{}", input);
             }
         }
     });
     commands.insert_resource(StreamSender(tx));
+}
+
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error>
+where
+    T: SizedSample + FromSample<f64>,
+{
+    let sample_rate = config.sample_rate.0 as f64;
+    let channels = config.channels as usize;
+    let c = 0.2 * (dsp::organ_hz(dsp::midi_hz(57.0)) + dsp::organ_hz(dsp::midi_hz(61.0)) + dsp::organ_hz(dsp::midi_hz(64.0)));
+    let c = c >> dsp::pan(0.0);
+    let c = c >> (dsp::chorus(0, 0.0, 0.01, 0.2) | dsp::chorus(1, 0.0, 0.01, 0.2));
+    let mut c = c
+        >> (dsp::declick() | dsp::declick())
+        >> (dsp::dcblock() | dsp::dcblock())
+        >> dsp::limiter_stereo((1.0, 5.0));
+    c.set_sample_rate(sample_rate);
+    c.allocate();
+    let mut next_value = move || assert_no_alloc(|| c.get_stereo());
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &mut next_value)
+        },
+        err_fn,
+        None,
+    )?;
+    stream.play()?;
+    std::thread::sleep(std::time::Duration::from_millis(50000));
+    Ok(())
+}
+
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> (f64, f64))
+where
+    T: SizedSample + FromSample<f64>,
+{
+    for frame in output.chunks_mut(channels) {
+        let sample = next_sample();
+        let left = T::from_sample(sample.0);
+        let right: T = T::from_sample(sample.1);
+
+        for (channel, sample) in frame.iter_mut().enumerate() {
+            if channel & 1 == 0 {
+                *sample = left;
+            } else {
+                *sample = right;
+            }
+        }
+    }
 }
 
 pub fn sort_by_order(
