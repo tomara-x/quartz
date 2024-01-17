68f57177a7f47245972fe64d4dfb9b6ece260fc0
tomara_x <86204514+tomara-x@users.noreply.github.com>
2024-01-17 19:45:29 +0200

    both types of commands

--- src/commands.rs
+++ src/commands.rs
@@ -7,17 +7,26 @@ pub fn command_parser(
     mut display: Query<&mut Text, With<CommandText>>,
     mut char_input_events: EventReader<ReceivedCharacter>,
 ) {
+    let text = &mut display.single_mut().sections[0].value;
     for event in char_input_events.read() {
-        if !event.char.is_control() {
-            display.single_mut().sections[0].value.push(event.char);
-        }
+        if !event.char.is_control() { text.push(event.char); }
     }
-    for c in keyboard_input.get_just_pressed() {
-        match c {
-            KeyCode::Escape => { display.single_mut().sections[0].value.clear(); },
-            KeyCode::Return => { display.single_mut().sections[0].value.clear(); },
-            KeyCode::Back => { display.single_mut().sections[0].value.pop(); },
+    if keyboard_input.just_pressed(KeyCode::Back) { text.pop(); }
+    if keyboard_input.just_pressed(KeyCode::Escape) { text.clear(); }
+    if keyboard_input.just_pressed(KeyCode::Return) {
+        // commands starting with :
+        match text.as_str() {
+            ":hi" => {
+                text.push_str("hiiiiiiiii");
+                info!(text);
+            },
             _ => {},
         }
+        text.clear();
+    }
+    // key commands
+    match text.as_str() {
+        "hi" => {text.clear();},
+        _ => {},
     }
 }
