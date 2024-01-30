035927089d2b8894f70c5c21985bbac65a19d01a
tomara_x <86204514+tomara-x@users.noreply.github.com>
2024-01-30 04:24:36 +0200

    don't store global transform
    and avoid panic when it's not there

--- src/connections.rs
+++ src/connections.rs
@@ -145,29 +145,33 @@ pub fn update_connection_arrows(
     mut aabb_query: Query<&mut Aabb>,
 ) {
     for (id, bh) in bh_query.iter() {
-        let i = trans_query.get(id).unwrap().translation().xy();
-        let f = trans_query.get(bh.wh).unwrap().translation().xy();
-        let ip = radius_query.get(id).unwrap().0;
-        let fp = radius_query.get(bh.wh).unwrap().0;
-        if let Ok(arrow_id) = arrow_query.get(bh.wh) {
-            let aabb = Aabb::enclosing([i.extend(1.), f.extend(1.)]).unwrap();
-            *aabb_query.get_mut(arrow_id.0).unwrap() = aabb;
-            let Mesh2dHandle(mesh_id) = mesh_ids.get(arrow_id.0).unwrap();
-            let mesh = meshes.get_mut(mesh_id).unwrap();
-            *mesh = Tri { i, f, ip, fp, b: 2. } .into();
+        if let (Ok(i), Ok(f)) = (trans_query.get(id), trans_query.get(bh.wh)) {
+            let i = i.translation().xy();
+            let f = f.translation().xy();
+            let ip = radius_query.get(id).unwrap().0;
+            let fp = radius_query.get(bh.wh).unwrap().0;
+            if let Ok(arrow_id) = arrow_query.get(bh.wh) {
+                let aabb = Aabb::enclosing([i.extend(1.), f.extend(1.)]).unwrap();
+                *aabb_query.get_mut(arrow_id.0).unwrap() = aabb;
+                let Mesh2dHandle(mesh_id) = mesh_ids.get(arrow_id.0).unwrap();
+                let mesh = meshes.get_mut(mesh_id).unwrap();
+                *mesh = Tri { i, f, ip, fp, b: 2. } .into();
+            }
         }
     }
     for (id, wh) in wh_query.iter() {
-        let f = trans_query.get(id).unwrap().translation().xy();
-        let i = trans_query.get(wh.bh).unwrap().translation().xy();
-        let fp = radius_query.get(id).unwrap().0;
-        let ip = radius_query.get(wh.bh).unwrap().0;
-        if let Ok(arrow_id) = arrow_query.get(id) {
-            let aabb = Aabb::enclosing([i.extend(1.), f.extend(1.)]).unwrap();
-            *aabb_query.get_mut(arrow_id.0).unwrap() = aabb;
-            let Mesh2dHandle(mesh_id) = mesh_ids.get(arrow_id.0).unwrap();
-            let mesh = meshes.get_mut(mesh_id).unwrap();
-            *mesh = Tri { i, f, ip, fp, b: 2. } .into();
+        if let (Ok(f), Ok(i)) = (trans_query.get(id), trans_query.get(wh.bh)) {
+            let f = f.translation().xy();
+            let i = i.translation().xy();
+            let fp = radius_query.get(id).unwrap().0;
+            let ip = radius_query.get(wh.bh).unwrap().0;
+            if let Ok(arrow_id) = arrow_query.get(id) {
+                let aabb = Aabb::enclosing([i.extend(1.), f.extend(1.)]).unwrap();
+                *aabb_query.get_mut(arrow_id.0).unwrap() = aabb;
+                let Mesh2dHandle(mesh_id) = mesh_ids.get(arrow_id.0).unwrap();
+                let mesh = meshes.get_mut(mesh_id).unwrap();
+                *mesh = Tri { i, f, ip, fp, b: 2. } .into();
+            }
         }
     }
 }
