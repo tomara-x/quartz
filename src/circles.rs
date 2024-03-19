e7b1024bdf5fa4321c0a0d7b6246ecce7d76c6a2
tomara_x <86204514+tomara-x@users.noreply.github.com>
2024-03-19 19:35:53 +0200

    correct behavior when deleting connected entitites
    this kinda closes(ish) #131, but we have a double despawn somewhere that
    i'd like to clean

--- src/circles.rs
+++ src/circles.rs
@@ -580,11 +580,12 @@ pub fn delete_selected(
     mut lost_wh_query: Query<&mut LostWH>,
 ) {
     let shift = keyboard_input.any_pressed([KeyCode::ShiftLeft, KeyCode::ShiftRight]);
-    if keyboard_input.just_pressed(KeyCode::Delete) && !shift {
+    if keyboard_input.just_pressed(KeyCode::Delete) {
         let mut order = false;
         for e in selected_query.iter() {
             if let Ok(holes) = holes_query.get(e) { // it's a circle
-                for hole in &holes.0 {
+                if shift { continue; }
+                for hole in &holes.0.clone() {
                     if let Ok(bh) = bh_query.get(*hole) {
                         let arrow = arrow_query.get(bh.wh).unwrap().0;
                         commands.entity(arrow).despawn();
@@ -603,6 +604,7 @@ pub fn delete_selected(
                             commands.entity(highlight.0).despawn();
                         }
                         lost_wh_query.get_mut(bh.wh_parent).unwrap().0 = true;
+                        holes_query.get_mut(bh.wh_parent).unwrap().0.retain(|x| *x != bh.wh);
                     } else if let Ok(wh) = wh_query.get(*hole) {
                         // don't remove things that will get removed later
                         if selected_query.contains(wh.bh_parent) { continue; }
@@ -622,6 +624,7 @@ pub fn delete_selected(
                         if let Ok(highlight) = highlight_query.get(wh.bh) {
                             commands.entity(highlight.0).despawn();
                         }
+                        holes_query.get_mut(wh.bh_parent).unwrap().0.retain(|x| *x != wh.bh);
                     }
                 }
                 order = true;
