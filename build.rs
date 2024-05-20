8110be58b55d1b030f2c69e7bcab7eb62e03d3ae
tomara_x <86204514+tomara-x@users.noreply.github.com>
2024-05-21 02:23:29 +0300

    handle the absence of git

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
 use std::process::Command;
 
 fn main() {
-    let hash = String::from_utf8(
-        Command::new("git")
-            .args(["rev-parse", "--short", "HEAD"])
-            .output().unwrap().stdout
-    ).unwrap();
-    println!("cargo:rustc-env=COMMIT_HASH={}", hash.trim());
+    let hash_utf8 = Command::new("git").args(["rev-parse", "--short", "HEAD"]).output();
+    if let Ok(hash_utf8) = hash_utf8 {
+        let hash = String::from_utf8(hash_utf8.stdout).unwrap();
+        println!("cargo:rustc-env=COMMIT_HASH={}", hash.trim());
+    } else {
+        println!("cargo:rustc-env=COMMIT_HASH=NO_GIT");
+    }
 }
